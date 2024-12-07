5ed2b00eb263e3fd19660fab3c4c1e5c30447b58
hamzashakoor119 <135787611+hamzashakoor119@users.noreply.github.com>
2024-12-07 14:13:25 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        border: "#e5e7eb", // Custom color for `border-border` class
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
